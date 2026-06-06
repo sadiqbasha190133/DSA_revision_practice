cc1248c6e2133772d8694cfffe913c28829c6094
Shaik Sadiq Basha <shaiksadiqbasha190133@gmail.com>
2026-06-06 18:19:31 +0530

    learned best time to buy and sell stock problem

---
+++ Arrays/best_time_to_buy_and_sell_stock.py
@@ -0,0 +1,15 @@
+class Solution:
+    def maxProfit(self, prices) -> int:
+        min_price = float("inf")
+        max_profit = 0
+
+        for price in prices:
+            if price < min_price:
+                min_price = price
+            elif price - min_price > max_profit:
+                max_profit = price - min_price
+            
+        return max_profit
+
+prices = [7,1,5,3,6,4]
+print(Solution().maxProfit(prices))
