22cf483ceb138045f53ccaae2ee600fd6ff490be
Shaik Sadiq Basha <shaiksadiqbasha190133@gmail.com>
2026-06-30 07:04:28 +0530

    solved grouping_anagrams problem

---
+++ Hashmaps/Grouping_anagrams.py
@@ -0,0 +1,19 @@
+
+class Solution:
+    def groupAnagrams(self, strs: list[str]) -> list[list[str]]:
+        freq = {}
+        for word in strs:
+            sorted_word = "".join(sorted(word))
+            if sorted_word not in  freq:
+                freq[sorted_word] = [sorted_word]
+            else:
+                freq[sorted_word].append(word)
+        ans = []
+        for value in freq.values():
+            ans.append(value)
+        return ans
+
+strs = ["eat","tea","tan","ate","nat","bat"]
+print(Solution().groupAnagrams(strs))
+        
+
